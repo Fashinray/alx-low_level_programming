a50158650b4f2a4ca2ff1faba6b6d9e6e9ad5b20
Fashinray <oluwatosin5383@gamil.com>
2022-09-12 20:00:31 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ */
+
+it main(void)
+{
+	char c;
+
+	int d;
+
+	c = 'a';
+	d = 0;
+	while
+		(d < 10) {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}

8971c71b58e8e59bdbbf4b20e773aeea6ce00622
Fashinray <oluwatosin5383@gamil.com>
2022-09-22 19:19:14 -0700

    0-main.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,18 +4,19 @@
  * _strcat - concatenates 2 strings
  * @dest: destination array
  * @src: source array
- *
- * return: destination array
+ * Return: destination array
  */
 
 char *_strcat(char *dest, char *src)
 {
 	char *ptr = dest;
+
 	while (*ptr != 0)
 		ptr++;
+
 	while (*src != 0)
 		*ptr++ = *src++;
 	*ptr = 0;
-	return (dest);
 
+	return (dest);
 }

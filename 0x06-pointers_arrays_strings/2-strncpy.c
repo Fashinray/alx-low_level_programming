2fa8f60901e86709d479d178d145c0ae748f6f6c
Fashinray <oluwatosin5383@gamil.com>
2022-09-22 21:56:07 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,19 +10,16 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int index = 0, src_len = 0;
 
-	i = 0;
-	while (i < n && src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	while (i < n)
-	{
-		dest[i] = '\10';
-		i++;
-	}
+	while (src[index++])
+		src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
 
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -23,7 +23,7 @@ char *cap_string(char *str)
 				str[index - 1] == '.' ||
 				str[index - 1] == '!' ||
 				str[index - 1] == '?' ||
-			        str[index - 1] == '"' ||
+				str[index - 1] == '"' ||
 				str[index - 1] == '(' ||
 				str[index - 1] == ')' ||
 				str[index - 1] == '{' ||
@@ -31,9 +31,8 @@ char *cap_string(char *str)
 				index == 0)
 			str[index] -= 32;
 
-		index++;
+		index;
 	}
 
 	return (str);
 }
-				

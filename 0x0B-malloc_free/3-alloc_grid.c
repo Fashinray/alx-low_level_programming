572e248587d95eafee9645c231dbe58b442973de
Fashinray <oluwatosin5383@gamil.com>
2022-10-03 20:29:44 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -34,11 +34,14 @@ int **alloc_grid(int width, int height)
 					free(array[i]);
 					i--;
 				}
+				free(array);
+				return (NULL);
 			}
-			return (array);
 		}
-		else
-		{
-			return (NULL);
-		}
+		return (array);
 	}
+	else
+	{
+		return (NULL);
+	}
+}

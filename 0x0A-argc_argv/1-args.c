5d4590f6f7de1a8f8ee18e0d74500b09b18f0c0d
Fashinray <oluwatosin5383@gamil.com>
2022-10-05 20:05:25 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print the name of the program
+ * @argc: Count arguments
+ * @argv: Argumets
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%i\n", argc - 1);
+
+	return (0);
+}
